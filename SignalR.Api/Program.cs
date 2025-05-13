202fc98b573c1ef2876ac9c8833f696c9179a488
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-14 00:25:33 +0400

    Api tarafında cors işlemlerini yaptım....

--- SignalR.Api/Program.cs
+++ SignalR.Api/Program.cs
@@ -8,7 +8,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSignalR();  
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(action =>
+{
+    action.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7168")
+        .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+}); 
 
 var app = builder.Build();
 
@@ -19,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
