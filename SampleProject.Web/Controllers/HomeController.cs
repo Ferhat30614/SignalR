eebf7f9128c7c46e007621f96a743afd3e0fb4e1
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-14 15:30:43 +0400

    SignIn işlemlerinin controller tarafını hallettim sorunsuz bir şekilde devam ediyor....

--- SampleProject.Web/Controllers/HomeController.cs
+++ SampleProject.Web/Controllers/HomeController.cs
@@ -1,29 +1,80 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SampleProject.Web.Models;
+using SampleProject.Web.Models.ViewModels;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace SampleProject.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> _logger,UserManager<IdentityUser> _userManager,SignInManager<IdentityUser> _signInManager) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();      
         }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var hasUser=await _userManager.FindByEmailAsync(model.Email);
+            if (hasUser == null)
+            {
+                ModelState.AddModelError(string.Empty, "username or password  is wrong");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(hasUser, model.Password, true, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "username or password  is wrong");
+                return View(model);
+            }
+
+
+
+
+            return RedirectToAction(nameof(Index));   
+        }
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();      
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if(!ModelState.IsValid) return View(model);
+
+            var createUser = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result=await _userManager.CreateAsync(createUser,model.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(model);
+            }
+
+
+            return RedirectToAction(nameof(SignIn));     // normal burda "SignIn" yazılabilirdi tabiki ama bu nameofun artıları var nameof(SignIn) kullandığında:
+
+            //Refactoring(yeniden adlandırma) yaptığında otomatik güncellenir.
+            //Yazım hatalarını compile - time’da fark eder.
         }
         public IActionResult ProductList()
         {
---
+++ SampleProject.Web/Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleProject.Web.Models.ViewModels
+{
+    public record SignUpViewModel([Required]string Email, [Required] string Password, [Required] string ConfirmPassword)
+    {
+    }
+}
