2ddfb65901102b92f3aba93dc2827b9609f1c9a6
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-14 12:20:55 +0400

    Yeni bir proje ve dbcontext işlemlerini ekledim

---
+++ SampleProject.Web/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleProject.Web.Models
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> option) :IdentityDbContext<IdentityUser,IdentityRole,string>(option)
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ SampleProject.Web/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleProject.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        [Precision(18,2)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; } = null!;
+    }
+}
