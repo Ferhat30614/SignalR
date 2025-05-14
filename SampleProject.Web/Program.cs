9a49f72a8a2713de4d1a470ef173ddfd63420265
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-14 13:36:59 +0400

    Context ve identityi dı container tarafında ekledim ve ilk migrationu oluşturdum herşey yolunda

--- SampleProject.Web/Program.cs
+++ SampleProject.Web/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SampleProject.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
