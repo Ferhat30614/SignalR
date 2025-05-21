7d7df28282b078339008a5638b502ec960269a86
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-22 01:01:55 +0400

    Covid Service sınıfını ekledim ...

---
+++ CovidChart.API/Models/CovidService.cs
@@ -0,0 +1,26 @@
+﻿using CovidChart.API.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CovidChart.API.Models
+{
+    public class CovidService(AppDbContext _context,IHubContext<CovidHub> _hubContext)
+    {
+        // IQueryable kullandım
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();   
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+
+            await _context.Covids.AddAsync(covid);      
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList","data");
+
+        }
+
+
+
+    }
+}
