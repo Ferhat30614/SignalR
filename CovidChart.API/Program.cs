fa0ef13a3eac32421dda436484e5b0af38b155e3
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-07-08 00:39:58 +0400

    Cors ayarları

--- CovidChart.API/Program.cs
+++ CovidChart.API/Program.cs
@@ -13,6 +13,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 builder.Services.AddScoped<CovidService>();
 
+
+builder.Services.AddCors(action =>
+{
+    action.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7035")
+        .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
@@ -26,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
